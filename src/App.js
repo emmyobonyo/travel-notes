e9b288fdb4082d9ac4f8755c3a52e8a888c57dcb
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-02-15 16:46:50 +0300

    Add styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './header/Header';
+import Item from './items/Item';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Item />
     </div>
   );
 }
