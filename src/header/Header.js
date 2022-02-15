1ed2c9ca05910b3f15156224b42aa5a527b86a7a
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-02-15 22:24:30 +0300

    Completed App

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from '../images/logo.png';
 import './header.css'
 
 function header() {
   return (
     <header>
       <div className="header-logo">
-        <img src={logo} className="header-image" alt="logo" />
+        <img src={`./images/logo.png`} className="header-image" alt="logo" />
         <p className="header-paragraph">my travel journal.</p>
       </div>
     </header>
--- src/items/Item.js
+++ src/items/Item.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import mountFuji from '../images/mountfuji.png';
 import AddLocationIcon from '@mui/icons-material/AddLocation';
 import './item.css';
 
 function Item({ data }) {
   return (
     <section className="section-component">
-      <img className="mount-fuji" src={mountFuji} alt="mount fuji"/>
+      <img className="mount-fuji" src={`./images/${data.imageUrl}`} alt={data.imageUrl}/>
       <div className="section-location">
         <div className="section-location-top">
           <AddLocationIcon sx={{ color: 'red' }} className="location-icon"/>
           <p className="country">{data.location}</p>
           <a href="https://www.google.com" className="google-location">View on Google Maps</a>
         </div>
-        <h1 className="image">Mount Fuji</h1>
-        <p className="date"><b>12 Jan, 2021 - 24 Feb, 2021</b></p>
+        <h1 className="image">{data.title}</h1>
+        <p className="date"><b>{data.startDate} - {data.endDate}</b></p>
         <p className="description">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+          {data.description}
         </p>
       </div>
     </section>
